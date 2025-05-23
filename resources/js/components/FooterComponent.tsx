369198925787ca0b2f4f5bda715b59e060b4eb2c
csw <zobirofkir19@gmail.com>
2025-05-23 10:49:58 +0100

    create footer component

--- resources/js/components/FooterComponent.tsx
+++ resources/js/components/FooterComponent.tsx
@@ -9,7 +9,7 @@ const FooterComponent = () => {
     <footer className="py-6 fixed bottom-0 left-0 w-full border-t dark:border-gray-800 dark:bg-black dark:text-white bg-white text-black">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">          
-          <div className="mt-4 md:mt-0 text-sm text-gray-600 dark:text-gray-400">
+          <div className="text-sm text-gray-600 dark:text-gray-400">
             © {currentYear} arraid. All rights reserved.
           </div>
         </div>
