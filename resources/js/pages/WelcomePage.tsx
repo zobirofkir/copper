d557893847d3da15fc2d900cf6101e195a3a49fb
csw <zobirofkir19@gmail.com>
2025-05-23 10:54:21 +0100

    create slider componen

--- resources/js/pages/WelcomePage.tsx
+++ resources/js/pages/WelcomePage.tsx
@@ -1,9 +1,11 @@
+import SliderComponent from "@/components/SliderComponent"
 import { Head } from "@inertiajs/react"
 
 const WelcomePage = () => {
   return (
     <div className='dark:text-white text-black'>
       <Head title="Accueil"/>
+      <SliderComponent />
     </div>
   )
 }
