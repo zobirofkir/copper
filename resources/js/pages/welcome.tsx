42a8cb713c8f0bfd91e5017f198f78017c4682b1
csw <zobirofkir19@gmail.com>
2025-05-23 10:09:13 +0100

    first commit

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -9,7 +9,7 @@ export default function Welcome() {
             <Head title="Welcome">
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-            </Head>
+            </Head> 
             <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
                 <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
                     <nav className="flex items-center justify-end gap-4">
