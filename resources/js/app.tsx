b1813e738114de92461eca4df0e25734961fd288
csw <zobirofkir19@gmail.com>
2025-05-23 10:19:31 +0100

    create header and footer component

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,6 +4,8 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,7 +15,30 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <>
+                {
+                    /**
+                     * Header Component
+                     */
+                }
+                <HeaderComponent />
+                
+                {
+                    /**
+                     * Body
+                     */
+                }
+                <App {...props} />
+                
+                {
+                    /**
+                     * Footer Component
+                     */
+                }
+                <FooterComponent />
+            </>
+        );
     },
     progress: {
         color: '#4B5563',
