b65e1ac5e4dfccf20a46bbf08843f5796424cc5f
csw <zobirofkir19@gmail.com>
2025-05-23 11:39:33 +0100

    create about component

--- resources/js/pages/WelcomePage.tsx
+++ resources/js/pages/WelcomePage.tsx
@@ -1,3 +1,4 @@
+import AboutComponent from "@/components/AboutComponent"
 import SliderComponent from "@/components/SliderComponent"
 import { Head } from "@inertiajs/react"
 
@@ -5,7 +6,19 @@ const WelcomePage = () => {
   return (
     <div className='dark:text-white text-black'>
       <Head title="Accueil"/>
+      {
+        /**
+         * Slider Component
+         */
+      }
       <SliderComponent />
+      
+      {
+        /**
+         * About Component
+         */
+      }
+      <AboutComponent />
     </div>
   )
 }
