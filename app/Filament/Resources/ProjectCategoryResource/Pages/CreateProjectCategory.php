4c2c9ed54647a3b99a1596e03935b54a9722adcf
csw <zobirofkir19@gmail.com>
2025-05-24 10:59:18 +0100

    create project category

---
+++ app/Filament/Resources/ProjectCategoryResource/Pages/CreateProjectCategory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ProjectCategoryResource\Pages;
+
+use App\Filament\Resources\ProjectCategoryResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateProjectCategory extends CreateRecord
+{
+    protected static string $resource = ProjectCategoryResource::class;
+}
---
+++ app/Filament/Resources/ProjectCategoryResource/Pages/EditProjectCategory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ProjectCategoryResource\Pages;
+
+use App\Filament\Resources\ProjectCategoryResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditProjectCategory extends EditRecord
+{
+    protected static string $resource = ProjectCategoryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
