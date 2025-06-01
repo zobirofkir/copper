00235f63faa4eeb085c7ca9bf2894e8b53448fec
csw <zobirofkir19@gmail.com>
2025-06-01 14:30:30 +0100

    change gallery title

--- resources/js/pages/GalleryPage.tsx
+++ resources/js/pages/GalleryPage.tsx
@@ -1,9 +1,11 @@
 import GalleryComponent from '@/components/GalleryComponent'
+import { Head } from '@inertiajs/react'
 import React from 'react'
 
 const GalleryPage = () => {
   return (
     <section>
+      <Head title='Gallery'/>
         <GalleryComponent />
     </section>
   )
