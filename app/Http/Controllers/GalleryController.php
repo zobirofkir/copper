e471696a7a3c462b90a99ee309d73c8c524c9447
csw <zobirofkir19@gmail.com>
2025-06-07 13:03:00 +0100

    call facade method in gallery controller

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -6,7 +6,23 @@ use App\Models\Gallery;
 use App\Models\GalleryCategory;
 use Illuminate\Http\Request;
 use App\Http\Resources\GalleryResource;
+use App\Services\Facades\GalleryFacade;
 
 class GalleryController extends Controller
 {
+    /**
+     * List All Galleries
+     */
+    public function index(Request $request)
+    {
+        return GalleryFacade::index($request);
+    }
+
+    /**
+     * Get ALl Galleries
+     */
+    public function getGalleries(Request $request)
+    {
+        return GalleryFacade::getGalleries($request);
+    }
 }
