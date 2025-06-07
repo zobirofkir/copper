0b04bfc8d1683ce49e7db3ed86744a3d517a06b6
csw <zobirofkir19@gmail.com>
2025-06-07 12:42:02 +0100

    add pages title

--- resources/js/pages/AboutPage.tsx
+++ resources/js/pages/AboutPage.tsx
@@ -1,13 +1,15 @@
 import AboutComponent from '@/components/AboutComponent'
+import { Head } from '@inertiajs/react'
 import React from 'react'
 
 const AboutPage = () => {
   return (
-        <section>
-            <div>
-                <AboutComponent />
-            </div>
-        </section>
+    <section>
+        <Head title='About Us'/>
+        <div>
+            <AboutComponent />
+        </div>
+    </section>
     )
 }
 
--- resources/js/pages/ContactPage.tsx
+++ resources/js/pages/ContactPage.tsx
@@ -1,9 +1,11 @@
 import ContactComponent from '@/components/ContactComponent'
+import { Head } from '@inertiajs/react'
 import React from 'react'
 
 const ContactPage = () => {
   return (
     <section className="bg-gradient-to-b from-white to-gray-50 dark:from-black dark:to-gray-900">
+      <Head title='Contact'/>
       <ContactComponent />
     </section>
   )
