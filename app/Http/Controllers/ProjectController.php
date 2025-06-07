60e44c6b74623bd828ff31a3d50b1feb28b0d63c
csw <zobirofkir19@gmail.com>
2025-06-07 12:56:18 +0100

    clean project controller

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,9 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\Project;
 use App\Models\ProjectCategory;
+use App\Services\Facades\ProjectFacade;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class ProjectController extends Controller
 {
+    /**
+     * List All Projects
+     */
+    public function index()
+    {
+        return ProjectFacade::index();
+    }
+
+    /**
+     * Filter Project
+     */
+    public function getProjects()
+    {
+        return ProjectFacade::getProjects();
+    }
+
+    /**
+     * Show Project by id
+     */
+    public function show($id)
+    {
+        return ProjectFacade::show($id);
+    }
 }
