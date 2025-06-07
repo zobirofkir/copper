bb4c9159fc1065f439726595ac8d880f76d77287
csw <zobirofkir19@gmail.com>
2025-06-07 12:57:07 +0100

    create service projvider

--- app/Providers/ProjectServiceProvider.php
+++ app/Providers/ProjectServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\Services\ProjectService;
 use Illuminate\Support\ServiceProvider;
 
 class ProjectServiceProvider extends ServiceProvider
@@ -11,7 +12,7 @@ class ProjectServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind('ProjectService', ProjectService::class);
     }
 
     /**
