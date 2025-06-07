07cc96b3f86f2283f9954f952235631b445a1566
csw <zobirofkir19@gmail.com>
2025-06-07 18:09:12 +0100

    create contact resource

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactRequest;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function store(ContactRequest $request)
+    {
+        //
+    }
 }
