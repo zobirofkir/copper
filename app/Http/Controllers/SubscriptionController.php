09649c82bc86b041b15aef6dd3d17065e22055f4
csw <zobirofkir19@gmail.com>
2025-06-10 10:08:45 +0100

    create subscription logic in Subscription Controller

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SubscriptionRequest;
+use App\Models\Subscription;
 use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
 {
-    //
+    public function store(SubscriptionRequest $request)
+    {
+        Subscription::create($request->validated());
+        return redirect()->back();
+    }
 }
