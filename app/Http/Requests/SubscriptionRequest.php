3c8770a7defb4a17a003f9cd8387d758acebef72
csw <zobirofkir19@gmail.com>
2025-06-10 10:07:38 +0100

    create subscription request

---
+++ app/Http/Requests/SubscriptionRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SubscriptionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "fullname" => "required|string|min:5|max:255",
+            "email" => "required|email|unique:subscriptions,email"
+        ];
+    }
+}
